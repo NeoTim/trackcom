27c2c9bdee5817565f5c16d2c8ae1c3787b1239e
hisimagination <joel.design@icloud.com>
2014-04-10 17:36:46 -0500

    Fix Data Base

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'trackcom',
+			'database'  => 'test1',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
