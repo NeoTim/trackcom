654dadb20d1b68d43c9cec0783f89cb6d0df3594
hisimagination <joel.design@icloud.com>
2014-04-10 17:35:59 -0500

    Fixed Hovering Issues

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'test1',
+			'database'  => 'trackcom',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
